e9adf9933d79fca482d4aa7038f3a5930848c851
Rachel Dahl <58237431+RaeDahl@users.noreply.github.com>
2025-06-02 13:09:40 -0500

    Add files via upload

---
+++ SandSnapQuery.py
@@ -0,0 +1,87 @@
+# A program to download data from the SandSnap database
+
+import requests
+import json
+import csv
+
+
+def SandSnapQuery(url : str, save_path : str, filter : dict, file_type : str = "json"):
+	"""
+	Description:
+	------------
+	Sends a query with defined filters to the SandSnap server,
+	downloads the response, and saves it in json format to a 
+	local file.
+
+	Parameters:
+	-----------
+	url : str
+		The url of the query page
+
+	save_path : str
+		The path to the output file. should be a .json or .csv
+	
+	filter : dict[str, dict[str, str]]
+		The filter parameters to be included in the query. Use json 
+		representation for layer definitions as described here: 
+		https://developers.arcgis.com/rest/services-reference/enterprise/query-feature-service/
+
+	file_type : str
+		can be either "csv" or "json", default is json
+		sets what file type to save output into
+
+	Returns:
+	--------
+	None, but does generate a file with output in the location specified by save_path
+
+	"""
+	try:
+		with open(save_path, "w") as output_file:
+	
+			# Request data from server
+			response = requests.get(url, params=filter)
+	
+			# Check for errors
+			if (response.status_code == 200):
+			
+				# Save data into output file
+				data = response.json()
+				data = data.get("features")
+
+				if (file_type == "json"):
+					json.dumps(data, output_file)
+
+				elif (file_type == "csv"):
+					writer = csv.DictWriter(output_file, fieldnames=data[0].keys())
+					writer.writeheader()
+					writer.writerows(data)
+
+				else:
+					print(f"Invalid file type {file_type} given. Saving as json instead.")
+
+			else:
+				print(f"Request failed with error code {response.status_code}.")
+
+	except Exception as e:
+
+		print(f"Program failed with error {e}.")
+
+
+
+
+
+
+### MAIN ###
+
+# url for SandSnap query page
+url = "https://services6.arcgis.com/rZL2YPlohtwSQBWu/arcgis/rest/services/survey123_402b0c9d9dfe4bcc8b4b7d6873c710fe_fieldworker/FeatureServer/query?layerDefs=%7B%220%22%3A%22country+%3D+%27USA%27%22%7D&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&outSR=&datumTransformation=&applyVCSProjection=false&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&returnIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&returnZ=false&returnM=false&sqlFormat=none&f=html&token=" 
+
+# path to file data is outputted in. Should be a json file.
+save_file_path = "output.json" 
+
+# filters for only data points with a calculated grain size and no errors
+params = {
+	"layerDefs": {"0":"calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error'"}
+} 
+
+SandSnapQuery(url, save_file_path, params)
---
+++ SandSnapQueryTests.py
@@ -0,0 +1,95 @@
+# Unit tests for SandSnap Query code
+
+from SandSnapQuery import SandSnapQuery
+import json
+
+CORRECT_QUERY_URL = "https://services6.arcgis.com/rZL2YPlohtwSQBWu/arcgis/rest/services/survey123_402b0c9d9dfe4bcc8b4b7d6873c710fe_fieldworker/FeatureServer/query?layerDefs=%7B%220%22%3A%22country+%3D+%27USA%27%22%7D&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&outSR=&datumTransformation=&applyVCSProjection=false&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&returnIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&returnZ=false&returnM=false&sqlFormat=none&f=html&token="
+BROKEN_URL = "https://services6.arcgis.com/rZL2YPlohtwSQBWu/arcgis/rest/services/survey123_402b0ce4bcc8b4b7d6873c710fe_fieldworker/FeatureServer/query?layerDefs=%7B%220%22%3A%22calc_grain_size+%3C%3E+%27Unknown+Grain+Size%27%22%7D&geometry=-91%2C+32%2C+-90%2C+33&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&outSR=&datumTransformation=&applyVCSProjection=false&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&returnIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&returnZ=false&returnM=false&sqlFormat=none&f=html&token="
+
+DEFAULT_FILTER = {
+	"layerDefs": {"0":"calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error'"}
+} 
+BROKEN_FILTER_1 = {
+	"layerDefs": "survey"} 
+BROKEN_FILTER_2 = {"layerDefs": 0}
+
+CANNON_BEACH_FILTER = {"layerDefs: {"0":"objectid=2120"}}
+OREGON_FILTER = {
+	"layerDefs": {"0":"calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error' AND location_state = 'OR'"}
+} 
+VICKSBURG_FILTER = {
+	"layerDefs": {"0":"calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error'"}
+	"geometry": {-91, 32, -90, 33}
+} 
+
+SAVE_PATH = "unit_test_output.json"
+
+
+
+# Test basic functionality
+SandSnapQuery(CORRECT_QUERY_URL, SAVE_PATH, DEFAULT_FILTER)
+if(os.path.exists(SAVE_PATH)):
+	print("Query and file save successful")
+else:
+	print("No file saved to save path")
+
+
+# Test error handling
+
+# Wrong URL
+SandSnapQuery(BROKEN_URL, SAVE_PATH, DEFAULT_FILTER)
+
+# Invalid parameters
+SandSnapQuery(CORRECT_QUERY_URL, SAVE_PATH, BROKEN_FILTER_1)
+SandSnapQuery(CORRECT_QUERY_URL, SAVE_PATH, BROKEN_FILTER_2)
+
+
+# Test filtering for specific data point
+
+SandSnapQuery(CORRECT_QUERY_URL, SAVE_PATH, CANNON_BEACH_FILTER)
+file = open(SAVE_PATH, r)
+data = json.load(file)
+if (data):
+
+	if(list(filter(lambda x:x ["objectid"] == "2120", data))):
+		print("Filtering by objectid successful)
+	else:
+		print("ID 2120 does not show up in result")
+else:
+	print("Filter by ID query unsuccessful, no data found")
+
+
+SandSnapQuery(CORRECT_QUERY_URL, SAVE_PATH, OREGON_FILTER)
+file = open(SAVE_PATH, r)
+data = json.load(file)
+if (data):
+
+	if(len(data) == 6):
+		print("Filtering by state successful)
+	elif (len(data) < 6):
+		print(f"Some data points missing. Only found {len(data)} valid sandsnaps in OR, there should be 6.")
+	else:
+		print(f"Extra data found. Found {len(data)} valid sandsnaps in OR, there should only be 6")
+else:
+	print("Filter by state query unsuccessful, no data found")
+
+
+SandSnapQuery(CORRECT_QUERY_URL, SAVE_PATH, VICKSBURG_FILTER)
+file = open(SAVE_PATH, r)
+data = json.load(file)
+if (data):
+
+	if(list(filter(lambda x:x ["objectid"] == "687", data))):
+		print("Sandsnap known to be in area found.")
+	else:
+		print("ID 687 does not show up in result, missing data")
+
+	if(len(data) == 75):
+		print("Correct number of sandsnaps found when filtering by geometry")
+	elif (len(data) < 6):
+		print(f"Some data points missing. Only found {len(data)} valid sandsnaps in range, there should be 75.")
+	else:
+		print(f"Extra data found. Found {len(data)} valid sandsnaps in area, there should only be 75")
+
+else:
+	print("Filter by geometry query unsuccessful, no data found")
