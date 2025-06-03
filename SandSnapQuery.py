51d13698ccd0beec20b543ded51e2ec98c850a36
Rachel Dahl <58237431+RaeDahl@users.noreply.github.com>
2025-06-03 12:09:46 -0500

    Fixed trailing whitespace

--- SandSnapQuery.py
+++ SandSnapQuery.py
@@ -44,10 +44,10 @@ def sand_snap_query(url : str, save_path : str, query_params : dict, file_type :
 
             # Request data from server
             response = requests.get(url, params=query_params, timeout=5)
-	
+
 	    # Check for errors
             if response.status_code == 200:
-			
+
 	        # Save data into output file
                 data = response.json()
                 data = data.get("features")
@@ -76,14 +76,14 @@ def sand_snap_query(url : str, save_path : str, query_params : dict, file_type :
 ### MAIN ###
 
 # url for SandSnap query page
-URL = "https://services6.arcgis.com/rZL2YPlohtwSQBWu/arcgis/rest/services/survey123_402b0c9d9dfe4bcc8b4b7d6873c710fe_fieldworker/FeatureServer/query?layerDefs=%7B%220%22%3A%22country+%3D+%27USA%27%22%7D&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&outSR=&datumTransformation=&applyVCSProjection=false&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&returnIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&returnZ=false&returnM=false&sqlFormat=none&f=html&token=" 
+URL = "https://services6.arcgis.com/rZL2YPlohtwSQBWu/arcgis/rest/services/survey123_402b0c9d9dfe4bcc8b4b7d6873c710fe_fieldworker/FeatureServer/query?layerDefs=%7B%220%22%3A%22country+%3D+%27USA%27%22%7D&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&outSR=&datumTransformation=&applyVCSProjection=false&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&returnIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&returnZ=false&returnM=false&sqlFormat=none&f=html&token="
 
 # path to file data is outputted in. Should be a json file.
-SAVE_FILE_PATH = "output.json" 
+SAVE_FILE_PATH = "output.json"
 
 # filters for only data points with a calculated grain size and no errors
 PARAMS = {
     "layerDefs": {"0":"calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error'"}
-} 
+}
 
 sand_snap_query(URL, SAVE_FILE_PATH, PARAMS)
