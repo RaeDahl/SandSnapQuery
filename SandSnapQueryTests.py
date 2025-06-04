737825eb469877d5950ec97eba8d8dc241310971
Rachel Dahl <58237431+RaeDahl@users.noreply.github.com>
2025-06-04 12:30:57 -0500

    Update SandSnapQueryTests.py

--- SandSnapQueryTests.py
+++ SandSnapQueryTests.py
@@ -10,24 +10,19 @@ import os
 
 from SandSnapQuery import sand_snap_query
 
-CORRECT_QUERY_URL = "https://services6.arcgis.com/rZL2YPlohtwSQBWu/arcgis/rest/services/survey123_402b0c9d9dfe4bcc8b4b7d6873c710fe_fieldworker/FeatureServer/query?layerDefs=%7B%220%22%3A%22country+%3D+%27USA%27%22%7D&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&outSR=&datumTransformation=&applyVCSProjection=false&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&returnIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&returnZ=false&returnM=false&sqlFormat=none&f=html&token="
-BROKEN_URL = "https://services6.arcgis.com/rZL2YPlohtwSQBWu/arcgis/rest/services/survey123_402b0ce4bcc8b4b7d6873c710fe_fieldworker/FeatureServer/query?layerDefs=%7B%220%22%3A%22calc_grain_size+%3C%3E+%27Unknown+Grain+Size%27%22%7D&geometry=-91%2C+32%2C+-90%2C+33&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&outSR=&datumTransformation=&applyVCSProjection=false&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&returnIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&returnZ=false&returnM=false&sqlFormat=none&f=html&token="
+CORRECT_QUERY_URL = "https://services6.arcgis.com/rZL2YPlohtwSQBWu/ArcGIS/rest/services/survey123_402b0c9d9dfe4bcc8b4b7d6873c710fe_fieldworker/FeatureServer/query"
+BROKEN_URL = "https://services6.arcgis.com/rZL2YPlohtwSQBWu/arcgis/rest/services/survey123_402b0ce4bcc8b4b7d6873c710fe_fieldworker/FeatureServer/query?layerDefs=%7B%220%22"
 
-DEFAULT_FILTER = {
-    "layerDefs": {"0":"calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error'"}
-}
-BROKEN_FILTER_1 = {
-    "layerDefs": "survey"} 
-BROKEN_FILTER_2 = {"layerDefs": 0}
+DEFAULT_FILTER = "calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error'"
+BROKEN_FILTER_1 = "survey"
+BROKEN_FILTER_2 = 0
 
-CANNON_BEACH_FILTER = {"layerDefs": {"0":"objectid=2120"}}
-OREGON_FILTER = {
-    "layerDefs": {"0":"calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error' AND location_state = 'OR'"}
-}
-VICKSBURG_FILTER = {
-    "layerDefs": {"0":"calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error'"},
-    "geometry": {"xmin":-91, "ymin":32, "xmax":-90, "ymax":33}
-}
+CANNON_BEACH_FILTER = "objectid=2120"
+OREGON_FILTER = "calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error' AND location_state = 'OR'"
+# VICKSBURG_FILTER = {
+#     "layerDefs": {"0":"calc_grain_size <> 'Unknown Grain Size' AND calc_grain_size IS NOT NULL AND unknown_error_flag = 'False' AND process_status <> 'Error'"},
+#     "geometry": {"xmin":-91, "ymin":32, "xmax":-90, "ymax":33}
+# }
 
 SAVE_PATH = "unit_test_output.json"
 CSV_SAVE_PATH = "unit_test_output.csv"
